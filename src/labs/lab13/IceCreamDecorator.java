61fb3e28b5f485c950f305978c59f808be773759
Julian Saldana <julianesaldana@yahoo.com>
2020-11-17 16:06:22 -0800

    added files

---
+++ src/labs/lab13/IceCreamDecorator.java
@@ -0,0 +1,5 @@
+package labs.lab13;// Because we extend IceCream, IceCreamDecorator is-a IceCream
+
+public abstract class IceCreamDecorator extends Dessert {
+ public abstract String getDescription();
+}
