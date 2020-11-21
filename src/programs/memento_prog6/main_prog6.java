319e9c069e9cb0f8ad0a0f1a5c1c5038d0f39b3d
Julian Saldana <julianesaldana@yahoo.com>
2020-11-20 23:29:27 -0800

    added memento classes and changed to BigInteger

--- src/programs/memento_prog6/main_prog6.java
+++ src/programs/memento_prog6/main_prog6.java
@@ -1,24 +1,27 @@
 package programs.memento_prog6;
 
+import java.math.BigInteger;
+
 public class main_prog6 {
     public static void main(String[] arg) {
         long start, end;
         double duration;
-        int f; // change this to BigInteger
+        BigInteger f; // changed this to BigInteger
 
         for (int i = 0; i < 1000; i++) {
             start = System.currentTimeMillis();
-            f = fibo(i);
+            BigInteger temp = new BigInteger(String.valueOf(i));
+            f = fibo(temp);
             end = System.currentTimeMillis();
             duration = (end - start) / 1000.0;
             System.out.printf("Fibo(%d): %12d (Secs: %5.3f)\n", i, f, duration);
         }
     }
 
-    public static int fibo(int n) {
-        if (n == 0 || n == 1)
-            return 1;
+    public static BigInteger fibo(BigInteger n) {
+        if (n.equals(BigInteger.ZERO) || n.equals(BigInteger.ONE))
+            return BigInteger.ONE;
         else
-            return fibo(n - 1) + fibo(n - 2);
+            return fibo(n.subtract(BigInteger.ONE)).add(fibo(n.subtract(BigInteger.TWO)));
     }
 }
