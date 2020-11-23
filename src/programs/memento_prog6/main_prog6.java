4163a78b8760d387cedce03bffedc438a2961103
Julian Saldana <julianesaldana@yahoo.com>
2020-11-22 16:21:13 -0800

    small changes, still need to implement memento to fibo

--- src/programs/memento_prog6/main_prog6.java
+++ src/programs/memento_prog6/main_prog6.java
@@ -4,6 +4,19 @@ import java.math.BigInteger;
 
 public class main_prog6 {
     public static void main(String[] arg) {
+        long start, end;
+        double duration;
+        BigInteger f; // changed this to BigInteger
+
+        for (int i = 0; i < 1000; i++) {
+            start = System.currentTimeMillis();
+            BigInteger temp = new BigInteger(String.valueOf(i));
+            f = fibo(temp);
+            end = System.currentTimeMillis();
+            duration = (end - start) / 1000.0;
+            System.out.printf("Fibo(%d): %12d (Secs: %5.3f)\n", i, f, duration);
+        }
+
         //testing out memento, originator, caretaker
         Originator originator = new Originator();
         CareTaker careTaker = new CareTaker();
@@ -22,21 +35,7 @@ public class main_prog6 {
         System.out.println("First saved State: " + originator.getState());
         originator.getStateFromMemento(careTaker.get(1));
         System.out.println("Second saved State: " + originator.getState());
-        //end of test, redid
-
-
-        long start, end;
-        double duration;
-        BigInteger f; // changed this to BigInteger
-
-        for (int i = 0; i < 1000; i++) {
-            start = System.currentTimeMillis();
-            BigInteger temp = new BigInteger(String.valueOf(i));
-            f = fibo(temp);
-            end = System.currentTimeMillis();
-            duration = (end - start) / 1000.0;
-            System.out.printf("Fibo(%d): %12d (Secs: %5.3f)\n", i, f, duration);
-        }
+        //end of test, will need to look at how to apply this to fibo function
     }
 
     public static BigInteger fibo(BigInteger n) {
