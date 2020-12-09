986148323cd710a21af77a2016f6bb593fd72595
Julian Saldana <julianesaldana@yahoo.com>
2020-12-08 18:54:56 -0800

    FINALLY DONE, all assignments added 100% working

--- src/labs/visitor_lab16/Main.java
+++ src/labs/visitor_lab16/Main.java
@@ -6,21 +6,22 @@ package labs.visitor_lab16;
 // Michael Gonzales
 // Julian Saldana
 // Demo time: 5:42 p.m.
+
 import java.util.ArrayList;
 
 class Main {
-  public static void main(String[] args) {
-    ArrayList<ItemElement> items = new ArrayList<>();
-    ShoppingCart shoppingCart = new ShoppingCart();
-    Fruit apple = new Fruit("Apple", 20, 3.05);
-    Magazine thrasher = new Magazine("Thrasher", "May 2020", 6.3);
-    items.add(apple);
-    items.add(thrasher);
-    for(ItemElement item : items){
-      item.accept(shoppingCart);
-      
+    public static void main(String[] args) {
+        ArrayList<ItemElement> items = new ArrayList<>();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        Fruit apple = new Fruit("Apple", 20, 3.05);
+        Magazine thrasher = new Magazine("Thrasher", "May 2020", 6.3);
+        items.add(apple);
+        items.add(thrasher);
+        for (ItemElement item : items) {
+            item.accept(shoppingCart);
+
+        }
+        double total = shoppingCart.getCartTotal();
+        System.out.println("Total Cost = " + total);
     }
-    double total = shoppingCart.getCartTotal();
-    System.out.println("Total Cost = " + total);
-  }
 }
